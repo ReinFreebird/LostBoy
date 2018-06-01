b10b30701ab7a718a309a7e5a476bb479af0c2a0
Bazu <Ozhkavosh@gmail.com>
2018-06-01 19:54:43 +0700

    SettingVolume

--- Assets/Script/MainMenuScript.cs
+++ Assets/Script/MainMenuScript.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour {
+    public GameObject windowsSetting;
+    public Slider VolumeMusic;
+    public Slider VolumeSound;
+    public AudioSource MusicSource;
+    public AudioSource SoundSource;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        //windowsSetting.SetActive(false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void changemusic()
+    {
+        MusicSource.volume =VolumeMusic.value ;
+    }
+
+    public void changesound()
+    {
+        SoundSource.volume = VolumeSound.value;
+    }
 }
