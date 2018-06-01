fa582976339c16e7141d4e4576a676867f61dd41
Bazu <Ozhkavosh@gmail.com>
2018-06-01 22:03:21 +0700

    Pause Setting

---
+++ Assets/Script/PauseSetting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSetting : MonoBehaviour
+{
+{
+    public GameObject windowsSetting;
+    public Slider VolumeMusic;
+    public Slider VolumeSound;
+    public AudioSource MusicSource;
+    public AudioSource SoundSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //windowsSetting.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changemusic()
+    {
+        MusicSource.volume = VolumeMusic.value;
+    }
+
+    public void changesound()
+    {
+        SoundSource.volume = VolumeSound.value;
+    }
+
+    public void closesetting(bool applied)
+    {
+        if (applied)
+
+        {
+
+        }
+        windowsSetting.SetActive(false);
+
+    }
+
+    public void opensetting()
+    {
+        windowsSetting.SetActive(true);
+    }
+}
