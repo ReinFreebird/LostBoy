0827962e140def2dc3fc5a17ac1fda8e3c6775e9
Aat <teamarcanasoul@gmail.com>
2018-06-01 20:04:39 +0700

    mainGameScene and script created

---
+++ Assets/Script/GameScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    public GameObject lobbyDisplay;
+    public GameObject kitchenDisplay;
+    public GameObject bedroomDisplay;
+    public GameObject bathroomDisplay;
+
+    public enum room { Lobby,Kitchen,Bedroom,Bathroom };
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void changeRoom(room room)
+    {
+        lobbyDisplay.SetActive(false);
+        kitchenDisplay.SetActive(false);
+        bedroomDisplay.SetActive(false);
+        bathroomDisplay.SetActive(false);
+        int x = (int)room;
+        switch (x)
+        {
+            case 0:
+                lobbyDisplay.SetActive(true);
+                break;
+            case 1:
+                kitchenDisplay.SetActive(true);
+                break;
+            case 2:
+                bedroomDisplay.SetActive(true);
+                break;
+            case 3:
+                bathroomDisplay.SetActive(true);
+                break;
+        }
+    }
+}
