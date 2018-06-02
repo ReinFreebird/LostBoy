f3673018e622883e5fb09d4de33791996944c1ee
Bazu <Ozhkavosh@gmail.com>
2018-06-02 13:42:46 +0700

    edit sound

--- Assets/Script/MainMenuScript.cs
+++ Assets/Script/MainMenuScript.cs
@@ -11,6 +11,16 @@ public class MainMenuScript : MonoBehaviour
     public Slider VolumeSound;
     public AudioSource MusicSource;
     public AudioSource SoundSource;
+    public AudioClip[] SoundClips;
+    /*click button choice 2
+     * ketemu key item 2
+     * Tidak ketemu cancel 2
+     * Apply Setting Jingle 2
+     * Buka kertas Book pag 1
+     * buka pintu open 1
+     * menang short triumphal
+     */
+
 
     private float currentMusicVolume;
     private float currentSoundVolume;
@@ -20,6 +30,7 @@ public class MainMenuScript : MonoBehaviour
         currentMusicVolume = playerPrefHandler.GetMusicVolume();
         currentSoundVolume = playerPrefHandler.GetSoundVolume();
         setSoundPrefs();
+        Sound(3);
     }
 
     // Update is called once per frame
@@ -43,7 +54,7 @@ public class MainMenuScript : MonoBehaviour
         if (applied)
 
         {
-            
+
             playerPrefHandler.applySetting(MusicSource.volume, SoundSource.volume);
             currentMusicVolume = playerPrefHandler.GetMusicVolume();
             currentSoundVolume = playerPrefHandler.GetSoundVolume();
@@ -58,6 +69,7 @@ public class MainMenuScript : MonoBehaviour
     {
         windowsSetting.SetActive(true);
         setSoundPrefs();
+        Sound(4);
     }
 
     public void StartGame()
@@ -71,6 +83,13 @@ public class MainMenuScript : MonoBehaviour
         SoundSource.volume = playerPrefHandler.GetSoundVolume();
         VolumeMusic.value = playerPrefHandler.GetMusicVolume();
         VolumeSound.value = playerPrefHandler.GetSoundVolume();
+
+    }
+
+    public void Sound(int sounds)
+    {
+    SoundSource.clip = SoundClips[sounds];
+        SoundSource.Play();
 
     }
 }
